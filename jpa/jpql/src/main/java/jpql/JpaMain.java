6aa94dd7a6b79f5d2eee84e3fbff4fa7702268ed
kim <rlaalswo2437@gmail.com>
2026-01-18 11:59:49 +0900

    Named 쿼리

--- jpa/jpql/src/main/java/jpql/JpaMain.java
+++ jpa/jpql/src/main/java/jpql/JpaMain.java
@@ -39,10 +39,9 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m From Member m join fetch m.team t";
-            List<Member> result = em.createQuery(query, Member.class)
-                    .setFirstResult(0)
-                    .setMaxResults(1)
+            String query = "select m From Member m where m.team = :team";
+            List<Member> members = em.createQuery(query, Member.class)
+                    .setParameter("team", teamA)
                     .getResultList();
 
             tx.commit();
